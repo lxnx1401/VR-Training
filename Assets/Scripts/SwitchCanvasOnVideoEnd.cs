75e3f0a4f5ef551873b8e3cd745b677fe95cf1e4
35241 <35241@hsrw.org>
2026-02-10 18:22:55 +0100

    controller drin (noch nicht an der hand)

---
+++ Assets/Scripts/SwitchCanvasOnVideoEnd.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class SwitchCanvasOnVideoEnd : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    [Header("Canvases")]
+    [SerializeField] private GameObject frontCanvas;   // Canvas mit Video
+    [SerializeField] private GameObject nextCanvas;    // Canvas danach
+
+    [Header("Optional")]
+    [SerializeField] private bool startVideoOnEnable = true;
+
+    private void Awake()
+    {
+        if (!videoPlayer) videoPlayer = GetComponent<VideoPlayer>();
+
+        if (!videoPlayer)
+        {
+            Debug.LogError("[SwitchCanvasOnVideoEnd] Kein VideoPlayer zugewiesen oder am gleichen Objekt vorhanden!");
+            enabled = false;
+            return;
+        }
+    }
+
+    private void OnEnable()
+    {
+        // Sicherstellen, dass am Anfang der richtige Canvas-Status aktiv ist
+        if (frontCanvas) frontCanvas.SetActive(true);
+        if (nextCanvas) nextCanvas.SetActive(false);
+
+        videoPlayer.loopPointReached += OnVideoFinished;
+
+        if (startVideoOnEnable)
+        {
+            videoPlayer.Stop();
+            videoPlayer.Play();
+        }
+    }
+
+    private void OnDisable()
+    {
+        videoPlayer.loopPointReached -= OnVideoFinished;
+    }
+
+    private void OnVideoFinished(VideoPlayer vp)
+    {
+        if (frontCanvas) frontCanvas.SetActive(false);
+        if (nextCanvas) nextCanvas.SetActive(true);
+    }
+}
